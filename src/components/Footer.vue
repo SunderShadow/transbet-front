d1b5a9960400bf9368263b1447eb81831372d7aa
Abrams0121 <denutenkov873@gmail.com>
2025-07-10 22:52:31 +0300

    Сделан переход по view страницам

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,36 +1,33 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-const currentTab = ref('account')
+
 </script>
 
 <template>
   <footer class="footer">
-    <div
+    <router-link
+        :to="{name: 'home'}"
         class="footer-item"
-        v-bind:class="{ active: currentTab === 'auction' }"
-        v-on:click="currentTab = 'auction'"
     >
       <svg viewBox="0 0 24 24">
         <path d="M3 21h2v-2h14v2h2v-2a2 2 0 0 0-2-2h-2v-4h1l2-4-2-4h-1V3h-4v2h-2V3H7v2H6l-2 4 2 4h1v4H5a2 2 0 0 0-2 2v2z"/>
       </svg>
       <span>Аукцион</span>
-    </div>
+    </router-link>
 
-    <div
+    <router-link
+        :to="{name: 'auction'}"
         class="footer-item"
-        v-bind:class="{ active: currentTab === 'bids' }"
-        v-on:click="currentTab = 'bids'"
     >
       <svg viewBox="0 0 24 24">
         <path d="M13 2L3 14h7v8l10-12h-7z"/>
       </svg>
       <span>Мои ставки</span>
-    </div>
+    </router-link>
 
     <div
         class="footer-item"
-        v-bind:class="{ active: currentTab === 'account' }"
-        v-on:click="currentTab = 'account'"
+
     >
       <svg viewBox="0 0 24 24">
         <path d="M12 12c2.7 0 5-2.3 5-5s-2.3-5-5-5-5 2.3-5 5 2.3 5 5 5zm0 2c-3.3 0-10 1.7-10 5v3h20v-3c0-3.3-6.7-5-10-5z"/>
@@ -70,7 +67,7 @@ const currentTab = ref('account')
   fill: currentColor;
 }
 
-.footer-item.active {
+.router-link-active {
   color: #7CC300;
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AuctionView from "@/views/AuctionView.vue";
+import AuctionSingleView from "@/views/AuctionSingleView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/auction',
+      name: 'auction',
+      component: AuctionView,
+    },
+    {
+      path: '/auctionSingle',
+      name: 'auctionSingle',
+      component: AuctionSingleView,
     }
   ],
 })
