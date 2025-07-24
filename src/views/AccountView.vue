282cbf93dbfbd8032f2e7771d0c6ec3aace19b1d
EmOtIoN <sunundershadow@gmail.com>
2025-07-24 07:43:08 +0300

    Поправлена страница аккаунта

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -19,19 +19,19 @@ function withdraw() {
 <template>
     <v-card class="pa-4" elevation="0">
       <v-row no-gutters align="center" justify="start">
-        <v-avatar size="80" class="mr-4">
+        <v-avatar size="80" class="mr-4" tile>
           <v-img :src="userImg"/>
         </v-avatar>
         <div class="text-subtitle-1 font-weight-medium">{{userName}}</div>
       </v-row>
 
-      <v-btn class="mt-4 mb-2" variant="outlined" color="success" block @click="updateInfo">
+      <v-btn class="mt-3" variant="outlined" color="success" block @click="updateInfo">
         Обновить информацию
       </v-btn>
 
 
-      <div class="text-body-1 mb-4">Лицевой счёт: {{balance}}</div>
-      <v-row>
+      <div class="mt-5">Лицевой счёт: {{balance}}</div>
+      <div class="d-flex mt-2">
         <v-btn variant="outlined" color="success" @click="withdraw">
           ВЫВЕСТИ
         </v-btn>
@@ -39,7 +39,7 @@ function withdraw() {
         <v-btn variant="outlined"  color="success" @click="withdraw">
           ВНЕСТИ
         </v-btn>
-      </v-row>
+      </div>
 
     </v-card>
 </template>
