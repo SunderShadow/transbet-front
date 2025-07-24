1bda0f1a49a886452678abd2eea1faf1ba87a2c6
EmOtIoN <sunundershadow@gmail.com>
2025-07-24 06:59:47 +0300

    Убран неиспользуемый компонент HomeView

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AuctionView from "@/views/AuctionView.vue";
-import AuctionSingleView from "@/views/AuctionSingleView.vue";
-import AccountView from "@/views/AccountView.vue";
+import AuctionView from "@/views/AuctionView.vue"
+import AuctionSingleView from "@/views/AuctionSingleView.vue"
+import AccountView from "@/views/AccountView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/auction',
       name: 'auction',
       component: AuctionView,
     },
